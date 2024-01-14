648a4df12879a31fc0935f51a50f9259e7965322
maksyuki <maksyuki@126.com>
2024-01-14 22:39:18 +0800

    feat: modify addr ctrl and gen modules

---
+++ rtl/addr_ctrl.sv
@@ -0,0 +1,145 @@
+// Copyright (c) 2023 Beijing Institute of Open Source Chip
+// sram is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//             http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+// EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+// MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+`include "axi_define.sv"
+
+module addr_ctrl (
+    input  logic                        aclk_i,
+    input  logic                        aresetn_i,
+    input  logic [`AXI4_ADDR_WIDTH-1:0] addr_i,
+    input  logic [                 2:0] asize_i,
+    input  logic [                 1:0] aburst_i,
+    input  logic [                 7:0] alen_i,
+    input  logic                        avalid_i,
+    output logic                        aready_o,
+    output logic [`AXI4_ADDR_WIDTH-1:0] addr_o,
+    output logic                        addr_last_o,
+    output logic                        addr_valid_o,
+    input  logic                        addr_ready_i
+);
+
+  logic s_addr_hdshk;
+  logic [2:0] s_asize_d, s_asize_q;
+  logic [1:0] s_aburst_d, s_aburst_q;
+  logic [7:0] s_alen_d, s_alen_q;
+  logic [`AXI4_ADDR_WIDTH-1:0] s_addrout_d, s_addrout_q;
+  logic s_addrout_en;
+  logic [7:0] s_cnt_d, s_cnt_q;
+  logic s_cnt_en;
+  logic [`AXI4_ADDR_OFT_WIDTH-1:0] s_oft_addr;
+  logic s_addrlast_d, s_addrlast_q;
+  logic s_addrvalid_d, s_addrvalid_q;
+  logic s_addrvalid_en;
+
+
+  assign aready_o     = ~s_addrvalid_q;
+  assign addr_o       = s_addrout_q;
+  assign addr_last_o  = s_addrlast_q;
+  assign addr_valid_o = s_addrvalid_q;
+  assign s_addr_hdshk = avalid_i & aready_o;
+
+  assign s_asize_d    = s_addr_hdshk ? asize_i : s_asize_q;
+  dffer #(3) u_asize_dffer (
+      aclk_i,
+      aresetn_i,
+      s_addr_hdshk,
+      s_asize_d,
+      s_asize_q
+  );
+
+  assign s_aburst_d = s_addr_hdshk ? aburst_i : s_aburst_q;
+  dffer #(2) u_aburst_dffer (
+      aclk_i,
+      aresetn_i,
+      s_addr_hdshk,
+      s_aburst_d,
+      s_aburst_q
+  );
+
+
+  assign s_alen_d = s_addr_hdshk ? alen_i : s_alen_q;
+  dffer #(4) u_alen_dffer (
+      aclk_i,
+      aresetn_i,
+      s_addr_hdshk,
+      s_alen_d,
+      s_alen_q
+  );
+
+
+  assign s_addrout_en = s_addr_hdshk || addr_ready_i;
+  always_comb begin
+    s_addrout_d = s_addrout_q;
+    if (s_addr_hdshk) begin
+      s_addrout_d = addr_i;
+    end else if (addr_ready_i) begin
+      s_addrout_d = {s_addrout_q[`AXI4_ADDR_WIDTH-1:12], s_oft_addr};
+    end
+  end
+  dffer #(`AXI4_ADDR_WIDTH) u_addrout_dffer (
+      aclk_i,
+      aresetn_i,
+      s_addrout_en,
+      s_addrout_d,
+      s_addrout_q
+  );
+
+
+  assign s_cnt_en = s_addr_hdshk || (s_addrvalid_q && addr_ready_i);
+  always_comb begin
+    s_cnt_d = s_cnt_q;
+    if (s_addr_hdshk) begin
+      s_cnt_d = alen_i;
+    end else if (s_addrvalid_q && addr_ready_i) begin
+      s_cnt_d = s_cnt_q - 1'b1;
+    end
+  end
+  dffer #(8) u_cnt_dffer (
+      aclk_i,
+      aresetn_i,
+      s_cnt_en,
+      s_cnt_d,
+      s_cnt_q
+  );
+
+  assign s_addrlast_d = s_cnt_q == '0;
+  dffr #(1) u_addrlast_dffr (
+      aclk_i,
+      aresetn_i,
+      s_addrlast_d,
+      s_addrlast_q
+  );
+
+
+  assign s_addrvalid_en = s_addr_hdshk || (addr_ready_i && s_addrlast_q);
+  always_comb begin
+    s_addrvalid_d = s_addrvalid_q;
+    if (s_addr_hdshk) begin
+      s_addrvalid_d = 1'b1;
+    end else if (addr_ready_i && s_addrlast_q) begin
+      s_addrvalid_d = 1'b0;
+    end
+  end
+  dffer #(1) u_addrvalid_dffer (
+      aclk_i,
+      aresetn_i,
+      s_addrvalid_en,
+      s_addrvalid_d,
+      s_addrvalid_q
+  );
+
+  addr_gen u_addr_gen (
+      .alen_i  (s_alen_q),
+      .asize_i (s_asize_q),
+      .aburst_i(s_aburst_q),
+      .addr_i  (s_addrout_q[11:0]),
+      .addr_o  (s_oft_addr)
+  );
+endmodule
--- rtl/addr_gen.sv
+++ rtl/addr_gen.sv
@@ -8,43 +8,46 @@
 // MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 // See the Mulan PSL v2 for more details.
 
+`include "axi_define.sv"
+
 module addr_gen (
-    input  logic [11:0] addr_i,     // no cross 4KB
-    input  logic [ 3:0] axlen_i,
-    input  logic [ 2:0] axsize_i,
-    input  logic [ 1:0] axburst_i,
-    output logic [11:0] addr_o
+    input  logic [                     3:0] alen_i,
+    input  logic [                     2:0] asize_i,
+    input  logic [                     1:0] aburst_i,
+    input  logic [`AXI4_ADDR_OFT_WIDTH-1:0] addr_i,
+    output logic [`AXI4_ADDR_OFT_WIDTH-1:0] addr_o
 );
 
-  logic [11:0] s_offset_addr, s_inc_addr, s_wrap_addr;
-  logic [11:0] s_mux_addr, s_res_addr;
+  logic [`AXI4_ADDR_OFT_WIDTH-1:0] s_offset_addr, s_inc_addr, s_wrap_addr;
+  logic [`AXI4_ADDR_OFT_WIDTH-1:0] s_mux_addr, s_res_addr;
 
+  // max 64 bits
   always_comb begin
     s_offset_addr = '0;
-    unique case (axsize_i)
-      `AXI4_BURST_SIZE_1BYTE:     s_offset_addr = addr_i[11:0];
-      `AXI4_BURST_SIZE_2BYTES:    s_offset_addr = {1'b0, addr_i[11:1]};
-      `AXI4_BURST_SIZE_4BYTES:    s_offset_addr = {2'b00, addr_i[11:2]};
-      `AXI4_BURST_SIZE_8BYTES:    s_offset_addr = {3'b000, addr_i[11:3]};
+    unique case (asize_i)
+      `AXI4_BURST_SIZE_1BYTE:     s_offset_addr = addr_i[`AXI4_ADDR_OFT_WIDTH-1:0];
+      `AXI4_BURST_SIZE_2BYTES:    s_offset_addr = {1'b0, addr_i[`AXI4_ADDR_OFT_WIDTH-1:1]};
+      `AXI4_BURST_SIZE_4BYTES:    s_offset_addr = {2'b00, addr_i[`AXI4_ADDR_OFT_WIDTH-1:2]};
+      `AXI4_BURST_SIZE_8BYTES:    s_offset_addr = {3'b000, addr_i[`AXI4_ADDR_OFT_WIDTH-1:3]};
       default s_offset_addr = '0;
     endcase
   end
 
-  assign s_inc_addr        = s_offset_addr + 1'b1;
-  assign s_wrap_addr[11:4] = s_offset_addr[11:4];
-  assign s_wrap_addr[3:0]  = (axlen_i & s_inc_addr[3:0]) | (~axlen_i & s_offset_addr[3:0]);
-  assign s_mux_addr        = axburst_i == `AXI4_BURST_TYPE_WRAP ? s_wrap_addr : s_inc_addr;
+  assign s_inc_addr = s_offset_addr + 1'b1;
+  assign s_wrap_addr[`AXI4_ADDR_OFT_WIDTH-1:4] = s_offset_addr[`AXI4_ADDR_OFT_WIDTH-1:4];
+  assign s_wrap_addr[3:0] = (alen_i & s_inc_addr[3:0]) | (~alen_i & s_offset_addr[3:0]);
+  assign s_mux_addr = aburst_i == `AXI4_BURST_TYPE_WRAP ? s_wrap_addr : s_inc_addr;
 
   always_comb begin
     s_res_addr = '0;
-    unique case (axsize_i)
+    unique case (asize_i)
       `AXI4_BURST_SIZE_1BYTE:  s_res_addr = s_mux_addr;
-      `AXI4_BURST_SIZE_2BYTES: s_res_addr = {s_mux_addr[10:0], 1'b0};
-      `AXI4_BURST_SIZE_4BYTES: s_res_addr = {s_mux_addr[9:0], 2'b00};
-      `AXI4_BURST_SIZE_8BYTES: s_res_addr = {s_mux_addr[8:0], 3'b000};
+      `AXI4_BURST_SIZE_2BYTES: s_res_addr = {s_mux_addr[`AXI4_ADDR_OFT_WIDTH-2:0], 1'b0};
+      `AXI4_BURST_SIZE_4BYTES: s_res_addr = {s_mux_addr[`AXI4_ADDR_OFT_WIDTH-3:0], 2'b00};
+      `AXI4_BURST_SIZE_8BYTES: s_res_addr = {s_mux_addr[`AXI4_ADDR_OFT_WIDTH-4:0], 3'b000};
       default s_res_addr = '0;
     endcase
   end
 
-  assign addr_o = axburst_i == `AXI4_BURST_TYPE_FIXED ? addr_i : s_res_addr;
+  assign addr_o = aburst_i == `AXI4_BURST_TYPE_FIXED ? addr_i : s_res_addr;
 endmodule
